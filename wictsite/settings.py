b23c162c7067f1b5ed70ae44d31cd8d246efd01f
fcoelho <none@none>
2012-06-08 03:44:20 -0300

    templates+css+js

--- wictsite/settings.py
+++ wictsite/settings.py
@@ -123,14 +123,14 @@ INSTALLED_APPS = (
     #'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # Uncomment the next line to enable the admin:
-    'django.contrib.admin',
-    # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
 	'registration',
 	'sendfile',
 	'wict',
 	'profiles',
+    # Uncomment the next line to enable the admin:
+    'django.contrib.admin',
+    # Uncomment the next line to enable admin documentation:
+    # 'django.contrib.admindocs',
 )
 
 AUTHENTICATION_BACKENDS = (
