1f7139353ab611e41ca3e63a9c419e03a93a7499
fcoelho <none@none>
2012-06-10 03:44:47 -0300

    botões mostram a página atual

--- wict/views.py
+++ wict/views.py
@@ -15,18 +15,18 @@ from wict.models import Article
 from wict.forms import ArticleForm
 
 def index(request):
-	return render(request, 'wict/index.html')
+	return render(request, 'wict/index.html', {'home_active': True})
 
 def articles(request):
-	return render(request, 'wict/articles.html')
+	return render(request, 'wict/articles.html', {'articles_active' : True})
 
 def registration(request):
-	return HttpResponse('registration')
+	return render(request, 'wict/registration.html', {'registration_active' : True})
 
 @require_submitter
 def submission(request):
 	article = Article.objects.filter(user=request.user)
-	context = {'article' : article}
+	context = {'article' : article, 'submission_active' : True}
 	return render(request, 'wict/submission.html', context)
 
 @require_submitter
@@ -38,12 +38,12 @@ def new_submission(request):
 	else:
 		form = ArticleForm()
 	
-	context = {'form' : form}
+	context = {'form' : form, 'submission_active' : True}
 	return render(request, 'wict/new_submission.html', context)
 
 @require_reviewer
 def review(request):
-	return render(request, 'wict/review.html')
+	return render(request, 'wict/review.html', {'review_active' : True})
 
 #Não faz sentido alguém tentar fazer login se já logou antes,
 #então se a pessoa estiver logada, redireciona pra outro lugar
