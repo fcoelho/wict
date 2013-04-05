864e31627c4c60a39884de94e9ac7ce1a283070f
fcoelho <none@none>
2013-04-05 16:43:28 -0300

    campos no admin e requirements.txt atualizado

--- wict/admin.py
+++ wict/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from wict.models import UserProfile
+from wict.models import UserProfile, Article
 
 class UserProfileAdmin(admin.ModelAdmin):
 	pass
 admin.site.register(UserProfile, UserProfileAdmin)
+
+class ArticleAdmin(admin.ModelAdmin):
+	list_display = ('user', 'title', 'topic')
+admin.site.register(Article, ArticleAdmin)
