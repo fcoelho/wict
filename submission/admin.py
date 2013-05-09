199b96ddd334f2e3dfc0349c81435e85b3b2c253
fcoelho <none@none>
2013-05-09 17:34:15 -0300

    link na lista de artigos pra finalizar a revisão
    --HG--
    extra : amend_source : de7221b8cee82f404401588e9349f8d9d054f995

--- submission/admin.py
+++ submission/admin.py
@@ -2,6 +2,12 @@ from django.contrib import admin
 from .models import Article
 
 class ArticleAdmin(admin.ModelAdmin):
-	list_display = ('author', 'title', 'topic')
+	def review(self, article):
+		return unicode('<a href="http://www.google.com">Avaliar</a>')
+	review.allow_tags = True
+	review.short_description = 'Avaliar'
+
+	list_display = ('author', 'title', 'topic', 'status', 'review')
+
 admin.site.register(Article, ArticleAdmin)
 
