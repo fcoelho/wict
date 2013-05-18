b04a1f3085805659cbd665fe5d5ac144feb1bf9a
fcoelho <none@none>
2013-05-18 11:22:03 -0300

    max_length para o status estava errado

--- submission/models.py
+++ submission/models.py
@@ -49,7 +49,7 @@ class Article(models.Model):
 	topic = models.CharField(max_length=2, choices=TOPIC_CHOICES, verbose_name='Assunto principal')
 	file = models.FileField(upload_to=article_upload_to, verbose_name='Arquivo')
 	reviews = models.ManyToManyField('website.WictUser', through='review.Review', related_name='reviews_set')
-	status = models.CharField(max_length=1, choices=STATUS_CHOICES, default='EN', verbose_name='Estado atual')
+	status = models.CharField(max_length=2, choices=STATUS_CHOICES, default='EN', verbose_name='Estado atual')
 
 	def __unicode__(self):
 		return self.title
